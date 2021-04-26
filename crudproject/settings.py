0b2e5490cccf4e4609b39f911aa0d26a5fd640d9
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2021-04-26 16:39:53 +0530

    erage database password

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'school',
         'USER': 'root',
-        'PASSWORD': 'ashu2468@$^*',
+        'PASSWORD': '*********',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
